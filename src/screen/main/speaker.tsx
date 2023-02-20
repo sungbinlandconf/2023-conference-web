4eed664281da479981110c1bcb744c823dd91ed5
jisungbin <ji@sungb.in>
2023-02-21 08:33:41 +0900

    fix text details

--- src/screen/main/speaker.tsx
+++ src/screen/main/speaker.tsx
@@ -76,7 +76,7 @@ export function MainSpeaker() {
                         speaker={{
                             name: "이기정",
                             description:
-                                "UDA를 사용하는 이점 중 하나는 상태 추적이 명확하고 쉬워짐입니다. 신규 서비스를 개발하면서 UDA를 적용하게 된 결정 과정과 적용 후 어떤 개선이 있었는지 이기정님이 공유합니다.",
+                                "UDA를 사용하는 이점 중 하나는 상태 추적이 명확하고 쉬워짐입니다. 신규 서비스를 개발하면서 UDA를 적용하게 된 결정 과정과 적용 후 어떤 개선이 있었는지 이기정 님이 공유합니다.",
                             image: "/assets/이기정.png",
                             sessionNumber: 1,
                             title: (
@@ -93,7 +93,7 @@ export function MainSpeaker() {
                         speaker={{
                             name: "윤희성",
                             description:
-                                "우리는 매번 Context를 사용합니다. 하지만 Context가 어떻게 구현되고 작동하는지는 자세히 알지 못합니다. 윤희성님이 Context의 비밀을 밝혀줍니다.",
+                                "우리는 매번 Context를 사용합니다. 하지만 Context가 어떻게 구현되고 작동하는지는 자세히 알지 못합니다. 윤희성 님이 Context의 비밀을 밝혀줍니다.",
                             image: "/assets/윤희성.png",
                             sessionNumber: 2,
                             title: (
@@ -110,7 +110,7 @@ export function MainSpeaker() {
                         speaker={{
                             name: "이정욱",
                             description:
-                                "나에게 맞는 회사 찾기란 사막에서 오아시스 찾기와 동일합니다. 여러 번의 이직 후 얻은 깨달음을 이정욱님이 공유합니다.",
+                                "나에게 맞는 회사 찾기란 사막에서 오아시스 찾기와 동일합니다. 여러 번의 이직 후 얻은 깨달음을 이정욱 님이 공유합니다.",
                             image: "/assets/이정욱.png",
                             sessionNumber: 3,
                             title: "이직! 그거 어떻게 하는 건데?",
@@ -121,7 +121,7 @@ export function MainSpeaker() {
                         speaker={{
                             name: "최우성",
                             description:
-                                "Compose로 얻을 수 있는 큰 장점 중 하나는 다양한 인터렉션을 구현하기 정말 쉬워졌다는 점 입니다. Canvas를 이용해 화려하고 재밌는 인터렉션을 쉽게 구현하는 방법을 최우성님이 소개합니다.",
+                                "Compose로 얻을 수 있는 큰 장점 중 하나는 다양한 인터렉션을 구현하기 정말 쉬워졌다는 점 입니다. Canvas를 이용해 화려하고 재밌는 인터렉션을 쉽게 구현하는 방법을 최우성 님이 소개합니다.",
                             image: "/assets/최우성.png",
                             sessionNumber: 4,
                             title: (
@@ -138,7 +138,7 @@ export function MainSpeaker() {
                         speaker={{
                             name: "류기민",
                             description:
-                                "Compose는 세상에 알려진 지 2년이 지나가고 있습니다. 다양한 기업들이 Compose를 채택하고 있고, 기민님의 회사도 마찬가지입니다. 류기민님은 Compose가 Stable이 되기 전부터 실무에 적용하셨고, 오랫동안 Compose를 써오시면서 느낀 점을 공유합니다.",
+                                "Compose는 세상에 알려진 지 2년이 지나가고 있습니다. 다양한 기업들이 Compose를 채택하고 있고, 기민 님의 회사도 마찬가지입니다. 류기민 님은 Compose가 Stable이 되기 전부터 실무에 적용하셨고, 오랫동안 Compose를 써오시면서 느낀 점을 공유합니다.",
                             image: "/assets/류기민.png",
                             sessionNumber: 5,
                             title: (
